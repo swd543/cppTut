80c6a0a67b165cbc7da90acdc6008e3c1684d236
Swapneel Datta <swapneel.datta@dattus.com>
2018-04-29 00:36:42 +0530

    C++ stuff

--- geeksforgeeks/programs/mergeSort.cpp
+++ geeksforgeeks/programs/mergeSort.cpp
@@ -43,77 +43,72 @@ procedure merge( var a as array, var b as array )
 end procedure
 
 */
-
-void display(string prefix, vector<int> *a){
-    cout<<prefix<<" --> ";
-    //display(*a);
-}
-/*
-void display(vector<int> a...){
-    for(int i=0;i<sizeof(a)/sizeof(vector<int>);i++){
-        for(const int v:a[i]){
-            cout<<v<<" ";
-        }
-        cout<<" / ";
+void display(vector<int> a){
+    for(const int v:a){
+        cout<<v<<" ";
     }
     cout<<endl;
 }
-*/
-void simple_printf(const char* fmt...)
-{
-    va_list args;
-    va_start(args, fmt);
- 
-    while (*fmt != '\0') {
-        if (*fmt == 'd') {
-            int i = va_arg(args, int);
-            std::cout << i << '\n';
-        } else if (*fmt == 'c') {
-            // note automatic conversion to integral type
-            int c = va_arg(args, int);
-            std::cout << static_cast<char>(c) << '\n';
-        } else if (*fmt == 'f') {
-            double d = va_arg(args, double);
-            std::cout << d << '\n';
-        }
-        ++fmt;
-    }
- 
-    va_end(args);
+
+void display(vector<int> *a){
+    display(*a);
 }
-void merge(vector<int> *left, vector<int> *right){
+
+void display(string prefix, vector<int> a){
+    cout<<prefix<<" --> ";
+    display(a);
+}
+
+void display(string prefix, vector<int> *a){
+    cout<<prefix<<" --> ";
+    display(a);
+}
+
+vector<int> merge(vector<int> left, vector<int> right){
     
     // Step 1: Copy the subarrays
 
-
     // Step 2: Until we reach the end of either left subarray or right subarray,
     // pick the larger among the elements from L and M and place them in the correct position at A[p..q]
+    int l=0,r=0,k=0;
+    vector<int> a;
+
+    for(;l<left.size() && r<right.size();k++){
+        if(left[l]<right[r]){
+            a.push_back(left[l++]);
+        }else{
+            a.push_back(right[r++]);
+        }
+    }
 
     // Step 3: When we run out of elements in either L or M, pick up the remaining elements and put in
     // A[p..q]
-}
-
-void mergesort(vector<int> *a){
-    if((*a).size()<=1){
-        return;
+    for(;l<left.size();l++){
+        a.push_back(left[l]);
     }
-    int mid=(*a).size()/2;
-    vector<int> left((*a).begin(), (*a).begin()+mid);
-    vector<int> right((*a).begin()+mid, (*a).end());
-    mergesort(&left);
-    mergesort(&right);
-    merge(&left, &right);
+    for(;r<right.size();r++){
+        a.push_back(right[r]);
+    }
+    return a;
 }
 
+vector<int> mergesort(vector<int> a){
+    if(a.size()<=1){
+        return a;
+    }
+    display(a);
+    int mid=a.size()/2;
+    vector<int> left(a.begin(), a.begin()+mid);
+    vector<int> right(a.begin()+mid, a.end());
+    left=mergesort(left);
+    right=mergesort(right);
+    return merge(left, right);
+}
 
 int main(){
     cout<<"This is a merge sort code"<<endl;
-    vector<int> a={10, 14, 19, 26, 0, 27, 31, 33, 35, 42, 44 };
-    vector<int> b={10, 14, 19, 26, 0, 27, 31, 33, 35, 42, 44 };
-
-    simple_printf("dcff", 3, 'a', 1.999, 42.5, 100000000000000999); 
-    //display("Initial array",&a);
-    //mergesort(&a);
-    //display(a, b);
+    vector<int> a={9,8,7,6,5,4,3,2,1,0};
+    display("Initial array",&a);
+    display("Sorted array",mergesort(a));
     return 0;
 }
